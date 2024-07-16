786760347ceb5f7381a4be32d598bbc1b5858ce4
rishabh-os <rishabhwanjari1@gmail.com>
2024-07-16 16:00:34 +0200

    The prodigal config returns

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,55 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+if vim.g.neovide then
+  vim.o.guifont = "VictorMono Nerd Font:h14"
+  vim.keymap.set('n', '<c-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<c-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<c-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<c-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<c-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<c-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.cmd [[set guicursor=a:ver25]]
+  end
+})
