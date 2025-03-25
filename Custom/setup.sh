87c972755bd1ae8ae59f6f59068fcb7bc05a78fc
rishabh-os <rishabhwanjari1@gmail.com>
2025-03-25 13:44:06 +0100

    Add custom setup script

---
+++ Custom/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ? Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# ? Install OhMyPosh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+# ? This will overwrite any existing .zshrc file
+curl --output ~/.zshrc "https://raw.githubusercontent.com/rishabh-os/Dot-Files/refs/heads/main/dot_zshrc"
+
+# ? Install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+
+# ? Install atuin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+# ? Install zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# ? Install eza manually
+wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+chmod +x eza
+mv eza $HOME/.local/bin/eza
+
+# ? Install chezmoi
+sh -c "$(curl -fsLS chezmoi.io/getlb)"
+chezmoi init https://github.com/rishabh-os/Dot-Files.git
+chezmoi apply
